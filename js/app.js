f990b461f38d8fe07273310948e6bc30b28e79fb
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-09-16 18:33:27 -0400

    9/16 updates to app.js

--- js/app.js
+++ js/app.js
@@ -13,41 +13,28 @@ let sheetAsJSON = "https://spreadsheets.google.com/feeds/list/1GozbvSSln3rtpnqxq
 
 const render = (projectsArray) => {
     console.log("Testing the projectsArray", projectsArray)
-    // Using forEach()
-    projectsArray.forEach(project => {
-        const $projectDiv= $("<div>");
+
+    // Using forEach() to append each project to page 
+    projectsArray.forEach((project, index) => {
+
+        const $projectDiv = $("<div>");
         const $titleText = $("<h3>");
         const $descriptionText = $("<p>");
         const $img = $("<img>");
-
         $projectDiv.append($titleText);
         $projectDiv.append($img);
         $projectDiv.append($descriptionText);
-        $projectDiv.addClass("proj-div")
+        $projectDiv.addClass(`proj-div-${index}`)
         $("article").append($projectDiv).addClass("projects-container");
 
-        $titleText.append(project.title).addClass("proj-title");
-        $img.append(project.image).attr("src", project.image).addClass("proj-img");
-        $descriptionText.append(project.description).addClass("proj-description");
+        $titleText.append(project.title).addClass(`proj-title-${index}`);
+        $img.append(project.image).attr("src", project.image).addClass(`proj-img-${index}`);
+        $descriptionText.append(project.description).addClass(`proj-description-${index}`);
         console.log(project);
     })
-    // $projectSection[0].addClass("proj-div1")
-    // $projectSection[1].addClass("proj-div2")
-    // $projectSection[2].addClass("proj-div3")
-    // $projectSection[3].addClass("proj-div4")
-
 }
 
-// $("article").append($projectSection);
 
-// $projectSection.append(project.title).addClass("card-title")
-// $projectSection.append(project.image).addClass("card-img-top")
-// $projectSection.append(project.description).addClass("card-text")
-// console.log(project)
-
-// (project.title).addClass("card-title")
-// (project.image).addClass("card-img-top")
-// (project.description).addClass("card-text")
 
 
 /* <div class="card" style="width: 18rem;">
@@ -79,23 +66,23 @@ const render = (projectsArray) => {
 
 //     console.log(projectsArray[i])
 // }
-            
+
 
 
 // Using AJAX to pull data
 
 $.ajax({ url: sheetAsJSON })
-    .then( data => {
-        //return a new array using .map
-        const projects = data.feed.entry.map( project => {
-            return  {
+    .then(data => {
+        //return a new array of objects using .map
+        const projects = data.feed.entry.map(project => {
+            return {
                 title: project.gsx$title.$t,
                 image: project.gsx$image.$t,
                 description: project.gsx$description.$t
             }
-       
+
         })
-    
+
         render(projects)
         console.log("This is the data, Title: ", data.feed.entry[0].gsx$title.$t)
         console.log("This is the data, Image: ", data.feed.entry[0].gsx$image.$t)
